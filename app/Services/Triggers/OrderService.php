f29018873f6edaaec6b9297f1d5b19b9e557e54c
Tornike Gochashvili <tokogochashvili887@gmail.com>
2025-07-16 20:30:28 +0400

    fixed delay issue for order job

--- app/Services/Triggers/OrderService.php
+++ app/Services/Triggers/OrderService.php
@@ -14,7 +14,7 @@ class OrderService
         $notifications = Notification::where('trigger', Trigger::ORDER_CREATED)->where('active', true)->get();
         $notifications->load('criteria');
         foreach ($notifications as $notification) {
-            $delay = ($notification->delay_m * 60) + ($notification->delay_h * 3600) + ($notification->delay_d * 86400);
+            $delay = ($notification->additional['delay_m'] * 60) + ($notification->additional['delay_h'] * 3600) + ($notification->additional['delay_d'] * 86400);
             OrderJob::dispatch($notification, $order)->delay(now()->addSeconds($delay));
         }
     }
@@ -24,7 +24,7 @@ class OrderService
         $notifications = Notification::where('trigger', Trigger::ORDER_CREATE_FINISHED)->where('active', true)->get();
         $notifications->load('criteria');
         foreach ($notifications as $notification) {
-            $delay = ($notification->delay_m * 60) + ($notification->delay_h * 3600) + ($notification->delay_d * 86400);
+            $delay = ($notification->additional['delay_m'] * 60) + ($notification->additional['delay_h'] * 3600) + ($notification->additional['delay_d'] * 86400);
             OrderJob::dispatch($notification, $order)->delay(now()->addSeconds($delay));
         }
     }
