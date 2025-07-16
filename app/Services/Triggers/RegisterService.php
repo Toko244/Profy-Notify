890db04ab1f0e3d33836b452b56d0d8b3066b7a3
Tornike Gochashvili <tokogochashvili887@gmail.com>
2025-07-16 20:37:35 +0400

    fixed delay

--- app/Services/Triggers/RegisterService.php
+++ app/Services/Triggers/RegisterService.php
@@ -14,7 +14,7 @@ class RegisterService
         $notifications = Notification::where('trigger', Trigger::REGISTER)->where('active', true)->get();
         $notifications->load('criteria');
         foreach ($notifications as $notification) {
-            $delay = ($notification->additional['delay_m'] * 60) + ($notification->additional['delay_m'] * 3600) + ($notification->additional['delay_m'] * 86400);
+            $delay = ($notification->additional['delay_m'] * 60) + ($notification->additional['delay_h'] * 3600) + ($notification->additional['delay_d'] * 86400);
             RegisterJob::dispatch($notification, $customer)->delay(now()->addSeconds($delay));
         }
     }
