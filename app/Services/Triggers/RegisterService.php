9c53e85466d4505d1d3948a32d2134a08da6eee7
Tornike Gochashvili <tokogochashvili887@gmail.com>
2025-07-16 20:31:46 +0400

    fixed delay issue on register job

--- app/Services/Triggers/RegisterService.php
+++ app/Services/Triggers/RegisterService.php
@@ -6,7 +6,6 @@ use App\Enums\Trigger;
 use App\Jobs\Triggers\RegisterJob;
 use App\Models\Customer;
 use App\Models\Notification;
-use Illuminate\Support\Facades\Log;
 
 class RegisterService
 {
@@ -15,13 +14,8 @@ class RegisterService
         $notifications = Notification::where('trigger', Trigger::REGISTER)->where('active', true)->get();
         $notifications->load('criteria');
         foreach ($notifications as $notification) {
-            $delay = ((int) ($notification->additional['delay_m'] ?? 0)) * 60
-                    + ((int) ($notification->additional['delay_h'] ?? 0)) * 3600
-                    + ((int) ($notification->additional['delay_d'] ?? 0)) * 86400;
-
-            $runAt = now()->addSeconds($delay);
-            Log::info("Dispatching RegisterJob. Delay: {$delay}s. Will run at: {$runAt}");
-            RegisterJob::dispatch($notification, $customer)->delay($runAt);
+            $delay = ($notification->additional['delay_m'] * 60) + ($notification->additional['delay_m'] * 3600) + ($notification->additional['delay_m'] * 86400);
+            RegisterJob::dispatch($notification, $customer)->delay(now()->addSeconds($delay));
         }
     }
 }
