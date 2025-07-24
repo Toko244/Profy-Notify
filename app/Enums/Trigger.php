9526af2d4c8dabbff2fa23588439e996a91cc77b
Tornike Gochashvili <tokogochashvili887@gmail.com>
2025-07-24 12:33:18 +0400

    removed order_service_service_selected_not_ordered trigger

--- app/Enums/Trigger.php
+++ app/Enums/Trigger.php
@@ -5,7 +5,6 @@ namespace App\Enums;
 enum Trigger: string{
     case REGISTER = 'register';
     case ORDER_CREATED = 'order_created';
-    case ORDER_SERVICE_SELECTED_NOT_ORDERED = 'order_service_selected_not_ordered';
     case ORDER_FINISHED = 'order_finished';
     case DAILY = 'daily';
     case WEEKLY = 'weekly';
